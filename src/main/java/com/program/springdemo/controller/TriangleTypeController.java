c0dd6bdbdd38aa725c4b665bea179bfbdf574ebf
pranehsagar13 <38105704+pranehsagar13@users.noreply.github.com>
2018-04-13 09:23:11 +0000

    Update TriangleTypeController.java

--- src/main/java/com/program/springdemo/controller/TriangleTypeController.java
+++ src/main/java/com/program/springdemo/controller/TriangleTypeController.java
@@ -1,29 +1,15 @@
-package com.program.springdemo.controller;
-
+package telstra.assessment.work.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.program.springdemo.service.TriangleTypeImpl;
-
-@RestControllerAdvice
-public class TriangleTypeController {
-    
-	
-	
-	@Autowired 
-	TriangleTypeImpl triangleTypeImpl;
-	
-	
-	public ResponseEntity<String> getTriangleBasedOnSide(@RequestParam int triangleside1, @RequestParam int triangleside2,@RequestParam int triangleside3)
-	{
-		
-		//String typeOfTriangle=triangleTypeImpl.getTriangleSide(int triangleside1,int triangleside2,int triangleside3);
-		
-		return null;
-		
-	}
-}
+import org.springframework.web.bind.annotation.RestController;
+import telstra.assessment.work.service.FibonnaciSeriesImpl;
+@RestController@RequestMapping("/api")public class FibonacciController {
+ @Autowired private FibonnaciSeriesImpl fibonacciService;
+ @RequestMapping(value = "/Fibonacci", method = RequestMethod.GET) public ResponseEntity<Long> getFibonacciNumber(@RequestParam("n") Object nthFibNum) throws IllegalArgumentException{  try {   Long fibonnaciNumber = new Long(nthFibNum.toString());
+   return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache())     .header("Pragma", "no-cache").body(fibonacciService.getFibonacci(fibonnaciNumber));  } catch (NumberFormatException invalidinput) {
+   throw new IllegalArgumentException("Invalid Input");  } }}
